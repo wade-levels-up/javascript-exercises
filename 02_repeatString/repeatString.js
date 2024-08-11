4a020aa146538aa7911c2b6c480bfd4f8082d946
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-08-11 11:17:41 +1000

    Complete excercise 02_repeatString all test pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, repeat) {
+    let newString = "";
 
+    if (repeat < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < repeat; i++) {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
