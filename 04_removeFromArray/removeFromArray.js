2b579e5363b180fcef74ce9d293fb0b5f480eaa2
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-08-12 09:45:36 +1000

    Refactor code in excercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,27 +1,15 @@
 
-const removeFromArray = function(array, valueToRemove) {
+function removeFromArray(array, ...args) {
 
-    // Load in current array as newArray so we can update it in the function
-    let newArray = array;
+    const newArray = [];
 
-    // Cycling through each argument provided other than the first (the array argument itself) 
-    // we check if the arg's value exists in the newArray, if it does we remove it
-    // then if it's removed we move the counter variable back one to make up for the
-    // length of the array now being shortened.
-    for (arg of arguments) { 
-        if (arg !== array) { 
-            for (let i = 0; i < array.length; i++) { 
-                if (arg === array[i]) { 
-                   newArray.splice(i, 1); 
-                   i--;
-                }
-            }
+    array.forEach((el) => {
+        if (!args.includes(el)){
+            newArray.push(el);
         }
-    }
+    })
 
     return newArray;
-
-};
-
+}
 // Do not edit below this line
 module.exports = removeFromArray;
