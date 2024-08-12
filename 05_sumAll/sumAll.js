8b51dc8e75010277860099359b5bfc090a38bd2f
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-08-12 10:26:26 +1000

    Complete excercise 05_sumAll all tests pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+ let sum = 0;
+ let largest;
+
+ // If either number is negative stop here and return error
+if (a < 0 || b < 0) {
+    return 'ERROR';
+}
+// if either number is not a whole number return error
+if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return 'ERROR';
+} 
+
+// Works out the largest number of the two arguments
+ if (a > b) {
+    largest = a;
+ } else if (a < b) {
+    largest = b;
+ }
+ 
+ for (let i = 0; i <= largest; i++) {
+    sum += i;
+ }
+
+ return sum;
 
 };
 
