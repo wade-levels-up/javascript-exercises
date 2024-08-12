1e826452648a59951e05767b2e6cdfac1fc627ad
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-08-12 14:48:11 +1000

    Complete excercise 07_tempConversion all test pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  return +((value - 32) * 5/9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  return +((value * 9/5) + 32).toFixed(1);
 };
 
 // Do not edit below this line
