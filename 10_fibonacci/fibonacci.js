bc9bff77781d56e68b67060de422b75c4e6d0f34
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-08-26 09:49:00 +1000

    Complete excercise 10_fibonacci with my solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+function generateFibonacci(positions) {
+    let seq = [1, 1];
+    for (let i = 1; i < positions; i++) {
+        seq.push(seq[i - 1] + seq[i]);
+    }
+    return seq;
+}
 
-};
+
+const fibonacci = function(seqPos) {
+    if (+seqPos === 0) {
+        return 0;
+    } else if (+seqPos < 0) {
+        return 'OOPS';
+    } else {
+    let fibonacci = generateFibonacci(+seqPos - 1);
+    return fibonacci[+seqPos - 1];
+    }
+}
+
+//  In this exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc.
 
 // Do not edit below this line
 module.exports = fibonacci;
