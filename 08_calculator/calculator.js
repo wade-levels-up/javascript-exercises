4c5228b27034e60321060994e052414972f2f8bb
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-08-25 14:16:44 +1000

    Complete excercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, cur) => sum + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, cur) => {
+    return sum * cur;
+  });
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) 
+    return -1;
+  else if (num == 0) 
+    return 1;
+  else {
+    return (num * factorial(num - 1));
+}
 };
 
 // Do not edit below this line
