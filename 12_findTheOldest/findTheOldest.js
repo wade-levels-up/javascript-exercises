41faeba0351d2799c323da3c746112d7bfcdd75b
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-08-26 08:35:03 +1000

    Update excercise 12_findTheOldest with best answer

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,41 +1,30 @@
-const findTheOldest = function(arr) {
-
-    let baseArr = arr;
-
-    // get the current year to be able to assign it where no yearOfDeath exists
-    const currentYear = (new Date()).getFullYear();
-
-    // break the objects into sub lists so we can use .includes on the array
-    for (let i = 0; i < arr.length; i++) {
-        baseArr[i] = [baseArr[i].name, baseArr[i].yearOfBirth, baseArr[i].yearOfDeath];
-    };
-
-    // where each sub-list contains undefined change the second index to currentYear
-    baseArr.forEach(element => {
-        if (element.includes(undefined)) {
-            element[2] = currentYear;
-        }
-    });
-
-    console.log(baseArr[0][0]);
-
-    let peopleAndAges = arr.reduce((obj, person, i) => {
-        obj[i] = {name: person[0], age: person[2] - person[1]};
-        return obj;
-        }, []).sort((a, b) => b.age - a.age);
-    
-
-    return peopleAndAges[0];
-
+// We provide year of birth and year of death to a function to
+// work out the person's age.
+// If the death value returns undefined it will equal false
+// in which case we assign death the value of the current date
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 }
 
-//     let peopleAndAges = arr.reduce((obj, person, i) => {
-//         obj[i] = {name: person.name, age: person.yearOfDeath - person.yearOfBirth}
-//         return obj;
-//     }, []).sort((a, b) => b.age - a.age);
+// We reduce the array down to a singular object and return
+// the specific person who is the oldest.
+// In order to do this we set 'oldest' as the accumulator, though it's
+// not necessarily the oldest - at least initially, but it will be.
 
-//     return peopleAndAges[0];
-// };
+// On each pass of the reduce method we create two variables, oldestAge
+// and personAge, we return the comparison between the two, which, if
+// the person being compared against the previous is older, they then
+// get returned becoming the new accumulator value.
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+        return oldestAge < personAge ? person : oldest;     
+    })
+};
 
 // Do not edit below this line
 module.exports = findTheOldest;
