503691f6d2f5dbbd6aa70b5f40df2d9b5c6a6f3a
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-08-25 15:23:45 +1000

    Complete excercise 09_palindromes with my solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
-};
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+         'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let array = string
+        .split('')
+        .map(item => item.toLowerCase())
+        .filter(item => alphabet.includes(item));
+
+    let arrayRv = string
+        .split('')
+        .reverse()
+        .map(item => item.toLowerCase())
+        .filter(item => alphabet.includes(item));
+
+    console.log({array}, {arrayRv});
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== arrayRv[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
