875e09da7f48584e1c7edfeefa6c7692fc69ebea
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-08-26 10:01:07 +1000

    Return my solution as it was

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -10,13 +10,14 @@ function generateFibonacci(positions) {
 const fibonacci = function(seqPos) {
     if (+seqPos === 0) {
         return 0;
-    } else if (+seqPos < 0) {
+    } else if (+seqPos < 0) {   
         return 'OOPS';
     } else {
     let fibonacci = generateFibonacci(+seqPos - 1);
     return fibonacci[+seqPos - 1];
     }
 }
+
 
 //  In this exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc.
 
